79f9344e7de86702e12bb7952af22a95d17c533a
hayar <malak.hayar.etu@univ-lille.fr>
2022-07-11 01:26:32 +0200

    starting to work on graphs

--- graph.c
+++ graph.c
@@ -1,1 +1,58 @@
 #include "graph.h"
+
+
+
+Graph new_graph(int vertices, int isOriented){
+    Graph element;
+    element = malloc(sizeof(element));
+    if (element == NULL){
+        printf("Error in memory allocation : FORCED EXIT");
+        exit(1);
+    }
+    element->isOriented = isOriented;
+    element->nbrVertices = vertices;
+    element->neighbours = malloc(vertices * sizeof(AdjencyList*));
+
+    if (element->neighbours == NULL){
+        printf("Error in memory allocation : FORCED EXIT");
+        exit(1);
+    }
+    int i;
+    for (i = 1; i <= element->nbrVertices ; i++){
+        element->neighbours[i - 1].being = NULL;
+        return element; 
+    }
+
+}
+
+int graph_is_empty(Graph* gr){
+    return (gr == NULL) ? 1 : 0;
+}
+
+static NodeList add_node(int x){
+    NodeList nl = malloc(sizeof(NodeListElement));
+    if (nl == NULL){
+        printf("Error in memory allocation : FORCED EXIT");
+        exit(1);
+    }
+    nl->value = x;
+    nl->next = NULL;
+    return nl;
+}
+
+void free_graph(Graph gr){
+    if (graph_is_empty(g) == 1){
+        printf("The graph doesn't exist, nothing to free");
+        return;
+    }
+
+    //.........
+}
+
+
+
+
+
+int main(void){
+    return 0;
+}
--- graph.h
+++ graph.h
@@ -1,7 +1,30 @@
 #ifndef _GRAPH_H_
+#define _GRAPH_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define _GRAPH_H_
+
+typedef struct NodeListElement{
+    int value;
+    struct Node* next;
+} NodeListElement, *NodeList;
+
+
+// for each node there is a adjency list containing all the nodes linked to him 
+typedef struct AdjencyList{
+    int numbrOfElements;
+    NodeListElement head; // head of the adjency list 
+} AdjencyList;
+
+typedef struct Graph{
+    int isOriented; // 1 if yes : 0 if no (i know i can define bools but kinda don't want to)
+    int nbrVertices; // direct "links" between nodes 
+    AdjencyList* neighbours;
+} GraphElement, *Graph; 
+
+Graph new_graph(int vertices, int isOriented);
+int graph_is_empty(Graph gr);
+void free_graph(Graph gr);
+
 #endif
