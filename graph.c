50fe04d7f4d633e44158e3460ab8c1e35742f294
hayar <malak.hayar.etu@univ-lille.fr>
2022-07-11 03:12:33 +0200

    more graphs functions

--- graph.c
+++ graph.c
@@ -11,7 +11,7 @@ Graph new_graph(int vertices, int isOriented){
     }
     element->isOriented = isOriented;
     element->nbrVertices = vertices;
-    element->neighbours = malloc(vertices * sizeof(AdjencyList*));
+    element->neighbours = malloc(vertices * sizeof(AdjencyListElement));
 
     if (element->neighbours == NULL){
         printf("Error in memory allocation : FORCED EXIT");
@@ -19,7 +19,7 @@ Graph new_graph(int vertices, int isOriented){
     }
     int i;
     for (i = 1; i <= element->nbrVertices ; i++){
-        element->neighbours[i - 1].being = NULL;
+        element->neighbours[i - 1].start = NULL; 
         return element; 
     }
 
@@ -29,7 +29,7 @@ int graph_is_empty(Graph* gr){
     return (gr == NULL) ? 1 : 0;
 }
 
-static NodeList add_node(int x){
+static NodeListElement* add_node(int x){
     NodeList nl = malloc(sizeof(NodeListElement));
     if (nl == NULL){
         printf("Error in memory allocation : FORCED EXIT");
@@ -40,14 +40,46 @@ static NodeList add_node(int x){
     return nl;
 }
 
+void add_edge(Graph gr, int source, int destination){
+    NodeListElement* node = add_node(destination);
+    node->next = gr->neighbours[src-1].start; // the vertice next to node is going to be equal to the start of the list 
+    gr->neighbours[src-1].start = n; // the start of the list is going to be "node"
+
+    if (g->isOriented == 0){ // if the graph isn't oriented, the same thing goes both ways
+        node = add_node(source);
+    node->next = gr->neighbours[destination-1].start;
+    gr->neighbours[destination-1].start = n;
+    }
+}
+
 void free_graph(Graph gr){
     if (graph_is_empty(g) == 1){
         printf("The graph doesn't exist, nothing to free");
         return;
     }
 
-    //.........
+    // if there's neighbours vertices
+    if (gr->neighbours){
+        int i;
+        for (i = 1; gr->nbrVertices + 1; i++){
+            NodeListElement* n = gr->neighbours[i - 1].begin;
+            while(n != NULL){
+                NodeListElement* tmp = n;
+                n = n->next;
+                free(tmp);
+            }
+        }
+        // freeing the whole adgency list after freeing every element of it
+        free(gr->neighbours);
+    }
+    // freeing the whole graph
+    free(gr);
 }
+
+void print_graph(Graph gr){}
+
+
+
 
 
 
--- graph.h
+++ graph.h
@@ -7,24 +7,27 @@
 
 typedef struct NodeListElement{
     int value;
-    struct Node* next;
-} NodeListElement, *NodeList;
+    struct NodeListElement* next;
+} NodeListElement;
 
 
 // for each node there is a adjency list containing all the nodes linked to him 
 typedef struct AdjencyList{
     int numbrOfElements;
-    NodeListElement head; // head of the adjency list 
-} AdjencyList;
+    NodeListElement start; // first element of the adgency list
+} AdjencyListElement, AdjencyList;
 
 typedef struct Graph{
     int isOriented; // 1 if yes : 0 if no (i know i can define bools but kinda don't want to)
-    int nbrVertices; // direct "links" between nodes 
-    AdjencyList* neighbours;
+    int nbrVertices; // the nodes 
+    AdjencyList neighbours;
 } GraphElement, *Graph; 
 
 Graph new_graph(int vertices, int isOriented);
 int graph_is_empty(Graph gr);
+static NodeListElement* add_node(int x);
 void free_graph(Graph gr);
+void add_edge(Graph gr, int source, int destination);
+void print_graph(Graph gr);
 
 #endif
